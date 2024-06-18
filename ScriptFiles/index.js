55fdd637afcee2afdc7729077f3fda2686cb4fb5
kingsley <uduma.kingsley74@gmail.com>
2024-06-18 23:19:54 +0100

    modification of the stylefile, inorder to make the color more dynami

--- ScriptFiles/index.js
+++ ScriptFiles/index.js
@@ -6,12 +6,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
       //color generating function
     let  getRandomColor = ()=>{
       var randomColor = Math.floor(Math.random() * 256)
-       return rgbColorGen =  `rgb(${randomColor}, ${randomColor}, ${randomColor})`
+      var randomColor2 = Math.floor(Math.random() * 256)
+      var randomColor3 = Math.floor(Math.random() * 256)
+       return rgbColorGen =  `rgb(${randomColor}, ${randomColor2}, ${randomColor3})`
     }
       //color change function
     changeColorBtn.addEventListener("click", ()=>{
         colorBox.style.backgroundColor = getRandomColor()
     })
+
+
 })
  
  
